cd5d309a411cd55e674c6bd420fac2ea957ee59a
seung-kang1 <162653908+seung-kang1@users.noreply.github.com>
2024-05-09 17:22:07 +0900

    Title: 비교 연산, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240509/비교 연산/comparison-operator.py
@@ -0,0 +1,34 @@
+tmp = input()
+arr = tmp.split()
+a = int(arr[0])
+b = int(arr[1])
+
+if a >= b:
+    print(1)
+else :
+    print(0)
+    
+if a > b:
+    print(1)
+else :
+    print(0)
+
+if a <= b:
+    print(1)
+else :
+    print(0)
+
+if a < b:
+    print(1)
+else :
+    print(0)
+
+if a == b:
+    print(1)
+else :
+    print(0)
+
+if a == b:
+    print(0)
+else :
+    print(1)
