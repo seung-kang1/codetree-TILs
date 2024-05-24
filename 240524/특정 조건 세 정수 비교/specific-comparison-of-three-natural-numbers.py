c36c275c45bc6fa0026e4b38798b4e2162e7e88d
seung-kang1 <162653908+seung-kang1@users.noreply.github.com>
2024-05-24 17:03:22 +0900

    Title: 특정 조건 세 정수 비교, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240524/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -0,0 +1,14 @@
+tmp = input()
+arr = tmp.split()
+a = int(arr[0])
+b = int(arr[1])
+c = int(arr[2])
+
+if a <= b and a <= c:
+    print(1, end = " ")
+else :
+    print(0, end = " ")
+if a == b and a == c :
+    print(1)
+else:
+    print(0)
